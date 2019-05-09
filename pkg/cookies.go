19adcda96583ef19ad4517f2d83abedf5bacc63f
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-05-09 23:28:51 +0100

    Add some initial func's currently in use, with tests

--- pkg/cookies.go
+++ pkg/cookies.go
@@ -1,10 +1,56 @@
 
 package pkg
 
-// add sums the two passed values.
-//
-// This function was created so the build scripts could be created before
-// anything else.
-func add(a int, b int) int {
-	return a + b
+import (
+	"bytes"
+	"log"
+	"regexp"
+	"time"
+	"unicode"
+)
+
+const (
+	POSITIVE_INT_CSV_PATTERN = "^([1-9][0-9]*,)*([1-9][0-9]*)$"
+)
+
+// StripWhitespace removes all white space from a string.
+func StripWhitespace(s string) string {
+	var buf bytes.Buffer
+	for _, r := range s {
+		if !unicode.IsSpace(r) {
+			buf.WriteRune(r)
+		}
+	}
+	return buf.String()
+}
+
+// ToUnixMilli returns the input Time as Unix milliseconds.
+func ToUnixMilli(t time.Time) int64 {
+	return t.UnixNano() / int64(time.Millisecond)
+}
+
+// LogIfErr checks if the input error is NOT nil. When true, the error is logged
+// and the check result is returned.
+func LogIfErr(err error) bool {
+	if err != nil {
+		log.Println("[ERROR] " + err.Error())
+		return true
+	}
+	return false
+}
+
+// WarnIfErr checks if the input error is NOT nil. When true, the error is
+// logged as a warning and the check result is returned.
+func WarnIfErr(err error) bool {
+	if err != nil {
+		log.Println("[WARNING] " + err.Error())
+		return true
+	}
+	return false
+}
+
+// IsPositiveIntCSV returns true if the input is a CSV of positive integers.
+func IsPositiveIntCSV(s string) bool {
+	match, _ := regexp.MatchString(POSITIVE_INT_CSV_PATTERN, s)
+	return match
 }
--- pkg/cookies_test.go
+++ pkg/cookies_test.go
@@ -2,12 +2,109 @@
 package pkg
 
 import (
+	"errors"
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/assert"
+	assert "github.com/stretchr/testify/assert"
+	require "github.com/stretchr/testify/require"
 )
 
-func TestAdd_1(t *testing.T) {
-	a := add(2, 3)
-	assert.Equal(t, 5, a)
+// ****************************************************************************
+// LogIfErr()
+// ****************************************************************************
+
+func TestLogIfErr_1(t *testing.T) {
+	act := LogIfErr(nil)
+	assert.False(t, act)
+	// Output:
+	//
+}
+
+func TestLogIfErr_2(t *testing.T) {
+	err := errors.New("Computer says no!")
+	act := LogIfErr(err)
+	assert.True(t, act)
+	// Output:
+	// [ERROR] Computer says no!
+}
+
+// ****************************************************************************
+// WarnIfErr()
+// ****************************************************************************
+
+func TestWarnIfErr_1(t *testing.T) {
+	act := WarnIfErr(nil)
+	assert.False(t, act)
+	// Output:
+	//
+}
+
+func TestWarnIfErr_2(t *testing.T) {
+	err := errors.New("Computer says no!")
+	act := WarnIfErr(err)
+	assert.True(t, act)
+	// Output:
+	// [warning] Computer says no!
+}
+
+// ****************************************************************************
+// StripWhitespace()
+// ****************************************************************************
+
+func TestStripWhitespace_0(t *testing.T) {
+	assert.Equal(t, "Rincewind", StripWhitespace("Rince \n\t\f\r wind"),
+		"Failed when whitespace in centre of given string")
+	assert.Equal(t, "Rincewind", StripWhitespace("\t \n\t \r\n\n\fRincewind"),
+		"Failed when whitespace at start of given string")	
+	assert.Equal(t, "Rincewind", StripWhitespace("Rincewind\r\n \t\t\f \r  \v\v"),
+		"Failed when whitespace at end of given string")	
+	assert.Equal(t, "Rincewind", StripWhitespace("\r\nRi \tn\tc\t\t ew\f \r  in\vd\v"),
+		"Failed when whitespace at the start, middle, and end of given string")
+	assert.Equal(t, "Rincewind", StripWhitespace("Rincewind"),
+		"Failed when non-whitespace string given")
+	assert.Equal(t, "", StripWhitespace(""),
+		"Failed when empty string given")
+	assert.Equal(t, "", StripWhitespace("\r\n \t\t \t\t \f \r  \v\v  "),
+		"Failed when whitespace only string given")
+}
+
+// ****************************************************************************
+// IsPositiveInt()
+// ****************************************************************************
+
+func TestIsPositiveIntCSV_1(t *testing.T) {
+	act := IsPositiveIntCSV("5")
+	assert.True(t, act)
+}
+
+func TestIsPositiveIntCSV_2(t *testing.T) {
+	act := IsPositiveIntCSV("1,2,3,4")
+	assert.True(t, act)
+}
+
+func TestIsPositiveIntCSV_3(t *testing.T) {
+	act := IsPositiveIntCSV("")
+	assert.False(t, act)
+}
+
+func TestIsPositiveIntCSV_4(t *testing.T) {
+	act := IsPositiveIntCSV("abc")
+	assert.False(t, act)
+}
+
+func TestIsPositiveIntCSV_5(t *testing.T) {
+	act := IsPositiveIntCSV("abc,efg,xyz")
+	assert.False(t, act)
+}
+
+// ****************************************************************************
+// ToUnixMilli()
+// ****************************************************************************
+
+func TestToUnixMilli_1(t *testing.T) {
+	aIn, err := time.Parse(time.RFC3339, "2019-04-15T21:50:33-00:00")
+	require.Nil(t, err)
+	aOut := ToUnixMilli(aIn)
+	assert.Equal(t, int64(1555365033000), aOut)
 }
