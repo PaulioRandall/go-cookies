6102be84358830f541142d4f24d0e1bae75c26f1
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-05-10 14:00:06 +0100

    Add function to return file data as a quoted string

---
+++ pkg/databind.go
@@ -0,0 +1,18 @@
+package pkg
+
+import (
+	"io/ioutil"
+	"strconv"
+)
+
+// fileToQuote returns the bytes of the input file as as a quoted string so it
+// may be embedded in source code. Use []byte(quotedString) to decode.
+func fileToQuote(varName string, file string) (string, error) {
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		return "", err
+	}
+
+	r := strconv.Quote(string(b))
+	return r, nil
+}
---
+++ pkg/databind_test.go
@@ -0,0 +1,32 @@
+package pkg
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	assert "github.com/stretchr/testify/assert"
+	require "github.com/stretchr/testify/require"
+)
+
+func TestFileToQuote(t *testing.T) {
+	f, err := ioutil.TempFile(".", "*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	data := []byte("What you see is all there is.")
+	_, err = f.Write(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a, err := fileToQuote("abc", f.Name())
+	require.Nil(t, err, "Did not expect error to be returned")
+
+	expect := []byte("\"What you see is all there is.\"")
+	actual := []byte(a)
+	assert.Equal(t, expect, actual)
+}
