8acc6414e828dafaf90186e3f974fcc386db4b85
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-05-16 16:54:50 +0100

    Use quickfiles pkg for injector pkg tests

--- injector/injector_test.go
+++ injector/injector_test.go
@@ -1,58 +1,25 @@
 package injector
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
 
 	assert "github.com/stretchr/testify/assert"
 	require "github.com/stretchr/testify/require"
+
+	q "github.com/PaulioRandall/go-cookies/quickfiles"
 )
 
-// createTmpDir creates a directory with the specified 'pattern' in the
-// specified 'dir' returning File.Name().
-func createTmpDir(t *testing.T, dir, pattern string) string {
-	d, err := ioutil.TempDir(dir, pattern)
-	require.Nil(t, err, "Error making temp directory")
-	return d
+func randomDir(t *testing.T) string {
+	f, err := ioutil.TempDir(".", "")
+	require.Nil(t, err)
+	return f
 }
 
-// createNamedTmpDir creates a directory with the specified 'name' in the
-// specified 'dir' returning File.Name().
-func createNamedTmpDir(t *testing.T, dir, name string) string {
-	n := dir + "/" + name
-	err := os.MkdirAll(n, 0777)
-	require.Nil(t, err, "Error making named temp directory or it's parents")
-	return n
-}
-
-// createTmpFile creates a file with the specified 'pattern' in the specified
-// 'dir' containing the specified 'content' returning File.Name().
-func createTmpFile(t *testing.T, dir, pattern, content string) string {
-	f, err := ioutil.TempFile(dir, pattern)
-	require.Nil(t, err, "Error creating temp file")
-	defer f.Close()
-	f.WriteString(content)
-	return f.Name()
-}
-
-// createNamedTmpFile creates a file with the specified 'name' in the specified
-// 'dir' containing the specified 'content' returning File.Name().
-func createNamedTmpFile(t *testing.T, dir, name, content string) string {
-	err := os.MkdirAll(dir, 0777)
-	require.Nil(t, err, "Error making parent directories of named temp file")
-
-	n := dir + "/" + name
-	b := []byte(content)
-	err = ioutil.WriteFile(n, b, 0777)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	require.Nil(t, err, "Error creating named temp file")
-	return n
+func removeDir(t *testing.T, dir string) {
+	err := os.RemoveAll(dir)
+	require.Nil(t, err)
 }
 
 const abc = `"abc": {
@@ -73,22 +40,26 @@ const tmp = `{
 }`
 
 func TestCompile(t *testing.T) {
-	d := createTmpDir(t, ".", "")
-	defer os.RemoveAll(d)
+	n := randomDir(t)
+	defer removeDir(t, n)
 
-	createNamedTmpFile(t, d, "abc.json", abc)
-
-	d2 := d + "/nested"
-	createNamedTmpFile(t, d2, "xyz.json", xyz)
-
-	tmp := createNamedTmpFile(t, d, "template", tmp)
-
-	inj := Injector{
-		Template:  tmp,
-		Resources: d,
+	tree := q.Tree{
+		Root: q.FilePath(n),
+		Files: map[q.FilePath]q.FileData{
+			"abc.json":        abc,
+			"nested/xyz.json": xyz,
+			"template":        tmp,
+		},
 	}
 
-	out := d + "/output"
+	tree.CreateFiles()
+
+	inj := Injector{
+		Template:  n + "/template",
+		Resources: n,
+	}
+
+	out := n + "/output"
 	err := inj.Compile(out)
 	require.Nil(t, err)
 
